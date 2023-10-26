0e1660896662ec9055dcfbf9f4de99661d768ba7
s-ync <149034018+s-ync@users.noreply.github.com>
2023-10-26 14:04:10 +0800

    RCD-Checker.lua

--- RCD-Checker.lua
+++ RCD-Checker.lua
@@ -1,7 +1,7 @@
 -- // RejectCharacterDeletions // --
 local Service = game.GetService
 local RCD = gethiddenproperty(Service(game, "Workspace"), "RejectCharacterDeletions")
-local Message = RDC and "Enabled, simple FE doesn't work" or "Disabled, simple FE does work"
+local Message = RCD and "Enabled, simple FE doesn't work" or "Disabled, simple FE does work"
 -- I just wanna make it 10 lines long lol
 local StarterGui = Service(game, "StarterGui") 
 StarterGui:SetCore("SendNotification", {

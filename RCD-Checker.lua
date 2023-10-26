9d3b64c5f16b76717c41951e1ae42eea39f30509
s-ync <149034018+s-ync@users.noreply.github.com>
2023-10-26 17:35:56 +0800

    RejectCharacterDeletions checker
    Checking if a game hidden property "RejectCharacterDeletions" is enabled or not

--- RCD-Checker.lua
+++ RCD-Checker.lua
@@ -2,7 +2,7 @@
 local Service = game.GetService
 local RCD = gethiddenproperty(Service(game, "Workspace"), "RejectCharacterDeletions")
 local Message = RCD and "Enabled, simple FE doesn't work" or "Disabled, simple FE does work"
--- I just wanna make it 10 lines long lol
+-- //
 local StarterGui = Service(game, "StarterGui") 
 StarterGui:SetCore("SendNotification", {
 	Title = "RejectCharacterDeletions is";

c63f29810caba7177090bb7eed75c4c1220180ca
Sync <149034018+s-ync@users.noreply.github.com>
2023-11-04 15:14:06 +0800

    ShiftLock for mobile users

--- Mobile-ShiftLock.lua
+++ Mobile-ShiftLock.lua
@@ -1,38 +1,41 @@
 -- Services
 local Service = game.GetService
-local Players = Service(game, "Players")
-local RunService = Service(game, "RunService")
-local CoreGui = Service(game, "CoreGui")
+local Players = Service(game, "Players");
+local RunService = Service(game, "RunService");
+local CoreGui = Service(game, "CoreGui");
+
 -- Variables
 local LocalPlayer = Players.LocalPlayer
 local Camera = workspace.CurrentCamera
+
 -- UI
-local ShiftLockGui = Instance.new("ScreenGui", CoreGui)
-local ShiftLockButton = Instance.new("ImageButton", ShiftLockGui)
+local ShiftLockGui = Instance.new("ScreenGui", CoreGui);
+local ShiftLockButton = Instance.new("ImageButton", ShiftLockGui);
 ShiftLockGui.Name = "SyncLock"
 ShiftLockGui.ZIndexBehavior = Enum.ZIndexBehavior.Sibling
 
-ShiftLockButton.BackgroundColor3 = Color3.fromRGB(255, 255, 255)
+ShiftLockButton.BackgroundColor3 = Color3.fromRGB(255, 255, 255);
 ShiftLockButton.BackgroundTransparency = 1.000
-ShiftLockButton.Position = UDim2.new(0.921914339, 0, 0.552375436, 0)
-ShiftLockButton.Size = UDim2.new(0.0636147112, 0, 0.0661305636, 0)
+ShiftLockButton.Position = UDim2.new(0.921914339, 0, 0.552375436, 0);
+ShiftLockButton.Size = UDim2.new(0.0636147112, 0, 0.0661305636, 0);
 ShiftLockButton.SizeConstraint = Enum.SizeConstraint.RelativeXX
 ShiftLockButton.Image = "http://www.roblox.com/asset/?id=182223762"
 
 -- Function
-local function ShiftLock()
-	local Character = LocalPlayer.Character or LocalPlayer.CharacterAdded:Wait()
-	local Humanoid = Character:WaitForChild("Humanoid")
-	local RootPart = Character:WaitForChild("HumanoidRootPart")
+local WasEnabled, DestroyScript = false
+local ShiftLock;
+ShiftLock = function()
+	local Character = LocalPlayer.Character or LocalPlayer.CharacterAdded:Wait();
+	local Humanoid = Character:WaitForChild("Humanoid");
+	local RootPart = Character:WaitForChild("HumanoidRootPart");
 	
 	local States = {
-		Disabled = "rbxasset://textures/ui/mouseLock_off@2x.png";
-		Enabled = "rbxasset://textures/ui/mouseLock_on@2x.png";
+		Disabled = "rbxasset://textures/ui/mouseLock_off@2x.png",
+		Enabled = "rbxasset://textures/ui/mouseLock_on@2x.png"
 	}
-	
 	local MaxLength = 900000
 	
-	local IsActive = false
+	local IsActive
 	local Connections = {}
 	
 	local UpdateImage = function(state)
@@ -41,38 +44,56 @@ local function ShiftLock()
 	
 	local Enable = function()
 		Humanoid.AutoRotate = false
-		UpdateImage("Enabled")
-		RootPart.CFrame = CFrame.new(RootPart.Position, Vector3.new(Camera.CFrame.LookVector.X * MaxLength, RootPart.Position.Y, Camera.CFrame.LookVector.Z * MaxLength))
-		Camera.CFrame = Camera.CFrame * CFrame.new(1.7, 0, 0)
+		UpdateImage("Enabled");
+		RootPart.CFrame = CFrame.new(RootPart.Position, Vector3.new(Camera.CFrame.LookVector.X * MaxLength, RootPart.Position.Y, Camera.CFrame.LookVector.Z * MaxLength));
+		Camera.CFrame = Camera.CFrame * CFrame.new(1.7, 0, 0);
 	end
 	
 	local Disable = function()
 		Humanoid.AutoRotate = true
-		UpdateImage("Disabled")
-		Camera.CFrame = Camera.CFrame * CFrame.new(-1.7, 0, 0)
+		UpdateImage("Disabled");
+		Camera.CFrame = Camera.CFrame * CFrame.new(-1.7, 0, 0);
 		pcall(function()
-			IsActive:Disconnect()
+			IsActive:Disconnect();
 			IsActive = nil
 		end)
 	end
 	
-	UpdateImage("Disabled")
+	UpdateImage("Disabled");
+	IsActive = WasEnabled and RunService.RenderStepped:Connect(Enable);
 	
 	Connections[#Connections + 1] = ShiftLockButton.MouseButton1Click:Connect(function()
-		if not IsActive then
-			IsActive = RunService.RenderStepped:Connect(Enable)
+		if (not IsActive) then
+			IsActive = RunService.RenderStepped:Connect(Enable);
+			WasEnabled = true
 		else
-			Disable()
+			Disable();
+			WasEnabled = false
 		end
-	end)
+	end);
+	
 	Connections[#Connections + 1] = Humanoid.Died:Connect(function()
-		Disable()
-		for i, v in ipairs(Connections) do
-			v:Disconnect()
+		LocalPlayer.CharacterAdded:Wait();
+		for i, v in next, Connections do
+			v:Disconnect();
 		end
-		LocalPlayer.CharacterAdded:Wait()
-		ShiftLock()
-	end)
+		pcall(function()
+			IsActive:Disconnect();
+			IsActive = nil
+		end);
+		ShiftLock();
+	end);
+	DestroyScript = function()
+		for i, v in next, Connections do
+			v:Disconnect();
+		end
+		if (IsActive) then
+			Disable();
+		end
+		ShiftLockGui:Destroy();
+	end
 end
 
-ShiftLock()
+ShiftLock();
+
+return { DestroyScript }
